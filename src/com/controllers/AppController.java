dc92b54cd5bfc90490dda3988a2fe9232d4b70d0
Hoshang Charania <hok14@pitt.edu>
2019-01-30 21:41:18 -0500

    MVC Application skeleton created

---
+++ src/com/controllers/AppController.java
@@ -0,0 +1,47 @@
+package com.controllers;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class AppController
+ */
+// @WebServlet("/AppController")
+public class AppController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public AppController() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		String htmlResponse="<html><h1>Welcome to Servlets</h1></html>";
+		//PrintWriter writer=response.getWriter();
+		// writer.append(htmlResponse).append(request.getContextPath());
+		RequestDispatcher rd=request.getRequestDispatcher("/views/index.html");
+		rd.forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
---
+++ src/com/dao/ApplicationDao.java
@@ -0,0 +1,14 @@
+package com.dao;
+
+import java.util.List;
+import com.beans.Player;
+
+
+
+public class ApplicationDao {
+	
+	public List<Player> searchPlayer(String searchPlayer){
+		
+	}
+
+}
